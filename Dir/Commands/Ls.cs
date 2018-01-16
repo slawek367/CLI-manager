6e47b256a596c7dede5313c10405ee3c0d49b9d4
slawek367 <slawomir.bando@gmail.com>
2018-01-16 03:27:11 +0100

    Added some options to LS command' :

--- Dir/Commands/Ls.cs
+++ Dir/Commands/Ls.cs
@@ -15,6 +15,18 @@ namespace Dir.Commands
             {
                 ListFiles();
             }
+            else if (args[0] == "-date")
+            {
+                ListFilesByDate();
+            }
+            else if (args[0] == "-size")
+            {
+                ListFilesBySize();
+            }
+            else if (args[0] == "-name")
+            {
+                ListFilesByName();
+            }
             else
             {
                 Console.WriteLine("This is ls command, just write ls to list folders and directories");
@@ -40,6 +52,43 @@ namespace Dir.Commands
                 Console.WriteLine(item);
             }
             Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void ListFilesByDate()
+        {
+
+        }
+
+        public static void ListFilesBySize()
+        {
+            DirectoryInfo dir = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+            var files = dir.GetFiles();
+            var directories = dir.GetDirectories();
+        }
+
+        public static void ListFilesByName()
+        {
+
+        }
+
+        //Test
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            // Add file sizes.
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+            }
+            return size;
         }
     }
 }
